d1557227f42baa7bd8d7ee7be1b1ed5076d393da
A22JavierLM <A22JavierLM@iessanclemente.net>
2023-02-17 14:10:37 +0100

    dsfdsfs

--- src/FormTest.java
+++ src/FormTest.java
@@ -16,34 +16,34 @@ public class FormTest{
         driver = new ChromeDriver();
     }
 
-    // @Test
-    // public void testCombo(){
-    //     driver.get("file:///home/sanclemente.local/a22javierlm/Documentos/probasWeb/CD_UD03_Selenium/src/index.html");
-    //     WebElement pinha = driver.findElement(By.name("pinha"));
-    //     WebElement sinPinha = driver.findElement(By.name("sin_pinha"));
-    //     pinha.click();
-    //     sinPinha.click();
-    //     assertEquals(true, sinPinha.isSelected());
-    //     assertEquals(false, pinha.isSelected());
-    // }
+    @Test
+    public void testCombo(){
+        driver.get("file:///home/sanclemente.local/a22javierlm/Documentos/probasWeb/CD_UD03_Selenium/src/index.html");
+        WebElement pinha = driver.findElement(By.name("pinha"));
+        WebElement sinPinha = driver.findElement(By.name("sin_pinha"));
+        pinha.click();
+        sinPinha.click();
+        assertEquals(true, sinPinha.isSelected());
+        assertEquals(false, pinha.isSelected());
+    }
 
-    // @Test
-    // public void testRadio(){
-    //     driver.get("file:///home/sanclemente.local/a22javierlm/Documentos/probasWeb/CD_UD03_Selenium/src/index.html");
-    //     WebElement fina = driver.findElement(By.xpath("//input[@name='boton' and @value='1']"));
-    //     WebElement gruesa = driver.findElement(By.cssSelector("[name='boton', value='2']"));
-    //     WebElement clasica = driver.findElement(By.cssSelector("[name='boton', value='3']"));
+    @Test
+    public void testRadio(){
+        driver.get("file:///home/sanclemente.local/a22javierlm/Documentos/probasWeb/CD_UD03_Selenium/src/index.html");
+        WebElement fina = driver.findElement(By.xpath("//input[@name='boton' and @value='1']"));
+        WebElement gruesa = driver.findElement(By.cssSelector("[name='boton', value='2']"));
+        WebElement clasica = driver.findElement(By.cssSelector("[name='boton', value='3']"));
 
-    //     fina.click();
-    //     gruesa.click();
-    //     clasica.click();
+        fina.click();
+        gruesa.click();
+        clasica.click();
 
-    //     assertEquals(false, fina.isSelected());
-    //     assertEquals(false, gruesa.isSelected());
-    //     assertEquals(true, clasica.isSelected());
+        assertEquals(false, fina.isSelected());
+        assertEquals(false, gruesa.isSelected());
+        assertEquals(true, clasica.isSelected());
 
 
-    // }
+    }
 
     @Test
     public void testComo(){
